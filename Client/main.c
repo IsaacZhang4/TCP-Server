f1a6c7580dd4e27aa9db9a7140edef1039299b33
Isaac Zhang <75555968+IsaacZhang4@users.noreply.github.com>
2025-02-01 15:19:11 -0500

    uploaded client files

---
+++ Client/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
+#include <conio.h>
+
+int main() {
+
+    char *ip = " 127.0.0.1";
+    int port = 5566;
+
+    int sock;
+    struct sockaddr_in addr;
+    socklen_t addr_size;
+    char buffer[1024];
+    int n;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sock < 0) {
+        perror("[-]Socket error");
+        exit(1);
+    }
+
+    printf("[+]TCP server socket created. \n");
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = port;
+    addr.sin_addr.s_addr = inet_addr(ip);
+
+    connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+    printf("Connected to the server.\n");
+
+    memset(buffer, 0, 1024);
+    strcpy(buffer, "Hello, This is client.");
+    printf("Client: %s\n");
+    send(sock, buffer, strlen(buffer), 0);
+
+    memset(buffer, 0, 1024);
+    recv(sock, buffer, sizeof(buffer), 0);
+    printf("Server: %s\n", buffer);
+
+    close(sock);
+
+    printf("Disconnected from server\n");
+
+    return 0;
+}
